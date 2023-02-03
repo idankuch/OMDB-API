62d02ccc9a5b39df1f31cd2990c37ef1d38bb6ac
idankuch <idan.ku@gmail.com>
2023-02-03 16:07:58 +0200

    movie item layout update - added poster

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./Card.css";
 
 const Card = function (props) {
-  return <div className={props.className}>{props.children}</div>;
+  const alert = () => console.log("movie clicked");
+
+  return (
+    <div className={props.className} onClick={alert}>
+      {props.children}
+    </div>
+  );
 };
 
 export default Card;
